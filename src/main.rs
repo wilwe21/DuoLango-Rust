8d56635a3fee5adb3aca01a7b151b30831f63c7e
wilwe <wilwe036@gmail.com>
2024-09-04 18:24:38 +0200

    change

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,12 @@ mod mainmenu;
 use crate::mainmenu::menu;
 
 fn on_activate(application: &gtk::Application) {
-    let mainBox = gtk::Box::new(gtk::Orientation::Vertical, 0);
-    let bbb = gtk::Box::new(gtk::Orientation::Vertical, 0);
-    mainBox.append(&bbb);
-    let butter = gtk::Button::builder()
-        .label("dupa")
-        .build();
-    bbb.append(&butter);
     let window = gtk::ApplicationWindow::builder()
         .title("DuoLango")
         .application(application)
         .child(&mainBox)
         .build();
-    unsafe {
-        butter.connect_clicked(move |_| BoxNewChild(&mainBox, &menu::MM(), &bbb));
-        window.show();
-    }
+    window.show();
 }
 
 fn BoxNewChild(parrent: &gtk::Box, child: &gtk::Box, ded: &gtk::Box) {
